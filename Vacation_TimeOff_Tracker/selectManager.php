86f112d693af66e8090ce7c1b5f2b468889e8560
jdevereaux0001 <143358192+jdevereaux0001@users.noreply.github.com>
2024-11-15 10:20:12 -0600

    Changes

--- Vacation_TimeOff_Tracker/selectManager.php
+++ Vacation_TimeOff_Tracker/selectManager.php
@@ -2,26 +2,27 @@
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 
-$con = mysqli_connect('localhost', 'root', '1Master01','home_info');
+$con = mysqli_connect('localhost', 'root', '1Master01','VTT');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql1 = "SELECT * FROM Cantiere WHERE home_id = '$txtHomeID'";
+$sql1 = "SELECT * FROM manager WHERE home_id = '$txtManagerID'";
 
 	$result = mysqli_query($sql1, $conn);
     
     $details = mysqli_fetch_array($result);
 
-    $savedNomeCantiere = $details["nomeCantiere"];
-    $savedCodiceCommessa = $details["codiceCommessa"];
-    $savedIndirizzoCantiere = $details["indirizzoCantiere"];
+    $savedManagerID = $details["managerID"];
+    $savedFirstName = $details["firstName"];
+    $savedLastName = $details["lastName"];
+    $savedEmail = $details["email"];
 
 var_dump($details);
 
 
 $result1 = $conn->query($sql1);
 
-echo($nomeCantiere);
+echo($savedManagerID, $savedFirstName, $savedEmail);
 
 ?>
---
+++ Vacation_TimeOff_Tracker/selectStudent.php
@@ -0,0 +1,32 @@
+<?php
+// database connection code
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+
+$con = mysqli_connect('localhost', 'root', '1Master01','VTT');
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql1 = "SELECT * FROM student WHERE home_id = '$txtStudentID'";
+
+	$result = mysqli_query($sql1, $conn);
+    
+    $details = mysqli_fetch_array($result);
+
+    $savedStudentID = $details["studentID"];
+    $savedFirstName = $details["firstName"];
+    $savedLastName = $details["lastName"];
+    $savedStartDate = $details["startDate"];
+    $savedEmail = $details["email"];
+    $savedVacationHrs = $details["vacationHrs"];
+    $savedSickHrs = $details["sickHrs"];
+    $savedPersonalHrs = $details["personalHrs"];
+
+var_dump($details);
+
+
+$result1 = $conn->query($sql1);
+
+echo($savedStudentID, $savedFirstName, $savedStartDate, $savedVacationHrs, $savedSickHrs, $savedPersonalHrs);
+
+?>
