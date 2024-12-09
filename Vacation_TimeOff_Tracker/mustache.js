f30c469c9498efd5d8bed36c3f22510798d10d32
jdevereaux0001 <143358192+jdevereaux0001@users.noreply.github.com>
2024-12-08 19:43:45 -0600

    SPRINT 5 changes

--- Vacation_TimeOff_Tracker/mustache.js
+++ Vacation_TimeOff_Tracker/mustache.js
@@ -8,6 +8,8 @@ function blockSpecialChars(event)
     event.returnValue = true;
 }
 function renderHello() {
+    const txtMaryvilleID = sessionStorage.getItem('txtMaryvilleID');
+    document.getElementById('txtMaryvilleID').textContent = txtMaryvilleID;
     const template = document.getElementById('template').innerHTML;
     const rendered = Mustache.render(template, { name: 'Jason' , year: 'txtStartDate' , ID: 'txtMaryvilleID' });
     document.getElementById('target').innerHTML = rendered;
