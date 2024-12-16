eff87acbba717849beff68bae72ef280ca809dde
jdevereaux0001 <143358192+jdevereaux0001@users.noreply.github.com>
2024-12-15 23:07:11 -0600

    SPRINT 6

--- Vacation_TimeOff_Tracker/PHP/selectStudent.php
+++ Vacation_TimeOff_Tracker/PHP/selectStudent.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // database connection code
 // $con = mysqli_connect('server', 'database_user', 'database_password','database');
 
